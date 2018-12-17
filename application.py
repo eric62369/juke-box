371008b7aa31898c241268f8c0a3f8db231a8efc
ericdyoon <yoone2@cs.washington.edu>
2018-12-17 02:14:49 -0800

    Some more updates

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import Button, Frame
 import musicplayer as mp
 
 # https://docs.python.org/3/library/tkinter.html#how-to-use-this-section
@@ -13,13 +14,21 @@ class App(tk.Frame):
         Initializes the UI for the Tk window object.
         '''
         super().__init__(master)
-        self.pack()
+        self.pack(expand=True, fill="both")
 
         self.musicPlayer = mp.MusicPlayer()
-        self.musicPlayer.readMusicFile("defaults")
-        self.testButton = tk.Button(self, text="Test", command=self.helloCallBack)
+        # self.musicPlayer.readMusicFile("defaults")
+
+        self.playbackFrame = tk.Frame(self, bg="blue")
+        self.playbackFrame.pack(side="top", fill="x")
+
+        self.playListsFrame = tk.Frame(self, bg="red")
+        self.playListsFrame.pack(side="left", fill="y")
+
+        self.testButton = tk.Button(self.playListsFrame, text="Test", bg="red", command=self.helloCallBack)
         self.testButton.pack(side="left")
-        self.testLabel = tk.Label(self, text="Yahs", relief=tk.RAISED)
+
+        self.testLabel = tk.Label(self.playbackFrame, text="Yahs", relief=tk.RAISED)
         self.testLabel.pack(side="left")
 
     def helloCallBack(self):
